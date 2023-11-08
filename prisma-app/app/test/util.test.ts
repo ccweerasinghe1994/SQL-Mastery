942a4fb82177828078d628a084b05790829f20b6
ccweerasinghe1994 <ccweerasinghe1994@gmail.com>
2023-11-08 15:49:43 +0530

    adding

--- prisma-app/app/test/util.test.ts
+++ prisma-app/app/test/util.test.ts
@@ -1,9 +1,41 @@
-import {toUpperCase} from "../src/util";
+import {databaseConnection} from "../src/databse";
+import {PrismaClient} from "@prisma/client";
 
 
-describe('Utils test suite', () => {
-    test('should return upper case string', () => {
-        const result = toUpperCase('hello');
-        expect(result).toBe('HELLO');
+let prisma: PrismaClient;
+
+describe('DB Tests', () => {
+    beforeAll(async () => {
+       prisma = await databaseConnection();
+        await prisma.user.deleteMany();
+        await prisma.customer.deleteMany();
+    });
+    test('Create a User',async () => {
+        try {
+
+        const user =await prisma.user.create({
+            data: {
+                email: 'ccweerasinghe1994@gmail.com',
+            }
+        });
+
+        expect(user).toBeTruthy();
+        }catch (e) {
+            console.log(e);
+        }
+    });
+    test('Create a Customer',async () => {
+        try {
+
+        const customer =await prisma.customer.create({
+            data:{
+                name:"Kevin"
+            }
+        });
+
+        expect(customer).toBeTruthy();
+        }catch (e) {
+            console.log(e);
+        }
     });
 });
